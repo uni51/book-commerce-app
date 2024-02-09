69ca5bca173cd9d0ff987bacffff2362ff0969d1
uni51 <unieye51@gmail.com>
2024-02-09 17:41:51 +0900

    11. NextAuth.jsで認証機能の実装をはじめよう

---
+++ src/app/lib/next-auth/options.ts
@@ -0,0 +1,26 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const nextAuthOptions: NextAuthOptions = {
+  debug: false,
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    // ...add more providers here
+  ],
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session: ({ session, user }) => {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+        },
+      };
+    },
+  },
+};
