8cbbcbf01cbd91b0f055514615687627505e7ab5
uni51 <unieye51@gmail.com>
2024-02-15 14:06:09 +0900

    14. prismaをグローバルインスタンスで定義しよう

--- src/app/lib/next-auth/options.ts
+++ src/app/lib/next-auth/options.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import prisma from "../prisma";
 
 export const nextAuthOptions: NextAuthOptions = {
   debug: false,
@@ -9,7 +10,6 @@ export const nextAuthOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
-    // ...add more providers here
   ],
   adapter: PrismaAdapter(prisma),
   callbacks: {
