88ba6d9edd36dcc1e339f85a9dc56cc8f1aecf07
uni51 <unieye51@gmail.com>
2024-02-26 18:23:54 +0900

    23. 全電子記事取得用の関数を用意しよう

--- src/app/lib/microcms/client.ts
+++ src/app/lib/microcms/client.ts
@@ -4,3 +4,9 @@ export const client = createClient({
   serviceDomain: process.env.NEXT_PUBLIC_SERVICE_DOMAIN!,
   apiKey: process.env.NEXT_PUBLIC_API_KEY!,
 });
+
+export const getAllbooks = async () => {
+  const allBooks = await client.getList({ endpoint: "bookcommerce" });
+
+  return allBooks;
+};
