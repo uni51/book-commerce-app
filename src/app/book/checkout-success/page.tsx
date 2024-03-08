e791688849fdbca676281ae9b8ac5235840e3a9d
uni51 <unieye51@gmail.com>
2024-03-08 09:28:46 +0900

    36. useEffectが2回発火されている問題を解決しよう

--- src/app/book/checkout-success/page.tsx
+++ src/app/book/checkout-success/page.tsx
@@ -22,7 +22,7 @@ const PurchaseSuccess = () => {
               body: JSON.stringify({ sessionId }),
             }
           );
-          console.log(await res.json());
+          // console.log(await res.json());
         } catch (err) {
           console.error(err);
         }
